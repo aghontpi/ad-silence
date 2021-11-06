2b107b3da5129c794db0067aad27b4517efbc6c5
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-06 09:36:32 +0530

    add protytpe screen to test audo funtions

--- app/src/main/java/bluepie/ad_silence/MainActivity.kt
+++ app/src/main/java/bluepie/ad_silence/MainActivity.kt
@@ -2,10 +2,26 @@ package bluepie.ad_silence
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById<Button>(R.id.vol_inc_tmp)).setOnClickListener {
+            var msg = "Volume Inc"
+            Log.v(TAG, msg)
+        }
+
+        (findViewById<Button>(R.id.vol_dec_tmp)).setOnClickListener {
+            var msg = "Volume Dec"
+            Log.v(TAG, msg)
+        }
     }
 }
