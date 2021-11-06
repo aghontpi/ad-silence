ac45151bf5154189355558477a28696a2c50f6dd
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-06 09:19:12 +0530

    initial commit

---
+++ app/src/main/java/bluepie/ad_silence/MainActivity.kt
@@ -0,0 +1,11 @@
+package bluepie.ad_silence
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
