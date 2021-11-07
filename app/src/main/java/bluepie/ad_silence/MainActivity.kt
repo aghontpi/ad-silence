89cf7ad59d8350f44cff77e3ae5652c6dc313cf4
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-07 12:50:24 +0530

    adjust volume, test volume Inc/volumne Dec

--- app/src/main/java/bluepie/ad_silence/MainActivity.kt
+++ app/src/main/java/bluepie/ad_silence/MainActivity.kt
@@ -1,5 +1,6 @@
 package bluepie.ad_silence
 
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,19 +10,22 @@ const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val audioManager: AudioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+
         (findViewById<Button>(R.id.vol_inc_tmp)).setOnClickListener {
-            var msg = "Volume Inc"
+            val msg = "Volume Inc"
             Log.v(TAG, msg)
+            audioManager.adjustVolume(AudioManager.ADJUST_RAISE,AudioManager.FLAG_PLAY_SOUND)
         }
 
         (findViewById<Button>(R.id.vol_dec_tmp)).setOnClickListener {
-            var msg = "Volume Dec"
+            val msg = "Volume Dec"
             Log.v(TAG, msg)
+            audioManager.adjustVolume(AudioManager.ADJUST_LOWER,AudioManager.FLAG_PLAY_SOUND)
         }
     }
 }
