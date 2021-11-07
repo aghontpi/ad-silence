53de8e188b5224a673a31c7c2380d5fa4b46c05e
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-07 14:35:40 +0530

    start & listen for notification posted and removed

--- app/src/main/java/bluepie/ad_silence/MainActivity.kt
+++ app/src/main/java/bluepie/ad_silence/MainActivity.kt
@@ -1,43 +1,58 @@
 package bluepie.ad_silence
 
+import android.content.Intent
 import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 
-const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val audioManager: AudioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+        val audioManager: AudioManager =
+            applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
 
         (findViewById<Button>(R.id.vol_inc_tmp)).setOnClickListener {
             val msg = "Volume Inc"
             Log.v(TAG, msg)
-            audioManager.adjustVolume(AudioManager.ADJUST_RAISE,AudioManager.FLAG_PLAY_SOUND)
+            audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND)
         }
 
         (findViewById<Button>(R.id.vol_dec_tmp)).setOnClickListener {
             val msg = "Volume Dec"
             Log.v(TAG, msg)
-            audioManager.adjustVolume(AudioManager.ADJUST_LOWER,AudioManager.FLAG_PLAY_SOUND)
+            audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND)
         }
 
         findViewById<Button>(R.id.volume_mute_tmp).setOnClickListener {
             val msg = "Volume Mute"
-            Log.v(TAG,msg)
+            Log.v(TAG, msg)
             audioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_PLAY_SOUND)
         }
 
         findViewById<Button>(R.id.volume_unmute_tmp).setOnClickListener {
             val msg = "Volume UnMute"
-            Log.v(TAG,msg)
+            Log.v(TAG, msg)
             audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_PLAY_SOUND)
         }
+
+        // NotificationListener Start
+        // notification listener doesn't need to be started
+//        applicationContext.startService(
+//            Intent(
+//                applicationContext,
+//                NotificationListener::class.java
+//            )
+//        )
+//        var settingActivity = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+//        startActivity(settingActivity)
+
     }
 }
--- app/src/main/java/bluepie/ad_silence/NotificationListener.kt
+++ app/src/main/java/bluepie/ad_silence/NotificationListener.kt
@@ -1,11 +1,40 @@
 package bluepie.ad_silence
 
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
 
-class NotificationListener : Service() {
-    override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
+
+class NotificationListener : NotificationListenerService() {
+    private val TAG = "NotificationListenerService"
+    var mConnected  = false
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.v(TAG,"listener created")
+    }
+
+    override fun onListenerConnected() {
+        Log.v(TAG, "notification listener connected")
+        mConnected = true
+    }
+
+    override fun onListenerDisconnected() {
+        Log.v(TAG, "notification listener disconnected")
+        mConnected = false
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        Log.v(TAG, "new notification posted: ${sbn.toString()}")
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {
+        super.onNotificationRemoved(sbn)
+        Log.v(TAG, "notification removed: ${sbn.toString()}")
+    }
+
+    override fun onLowMemory() {
+        Log.v(TAG, "low memory trigger")
     }
 }
