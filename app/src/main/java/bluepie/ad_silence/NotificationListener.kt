cf1a9077bf1ad77ea86f5326ef6efcb9a7a3fdc6
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-07 13:38:45 +0530

    notification listener init

---
+++ app/src/main/java/bluepie/ad_silence/NotificationListener.kt
@@ -0,0 +1,11 @@
+package bluepie.ad_silence
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class NotificationListener : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
