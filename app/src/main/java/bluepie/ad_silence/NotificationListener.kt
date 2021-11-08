62823ff6f4d08b826a5784daa91d2af847fd711a
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-08 09:07:31 +0530

    complete notfication parser and mute/unmute based on currently playing song

--- app/src/main/java/bluepie/ad_silence/NotificationListener.kt
+++ app/src/main/java/bluepie/ad_silence/NotificationListener.kt
@@ -1,21 +1,23 @@
 package bluepie.ad_silence
 
+import android.media.AudioManager
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
-import java.util.ArrayList
-import java.util.LinkedList
-import java.util.Objects
+import java.util.*
 
 
-sealed class NotificationListener : NotificationListenerService() {
+class NotificationListener : NotificationListenerService() {
     private val TAG = "NotificationListenerService"
-    var mConnected  = false
+    var mConnected = false
     var isMuted = false
+    var audioManager: AudioManager? = null
 
     override fun onCreate() {
         super.onCreate()
-        Log.v(TAG,"listener created")
+        audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+        Log.v(TAG, "listener created")
+
     }
 
     override fun onListenerConnected() {
@@ -32,48 +34,66 @@ sealed class NotificationListener : NotificationListenerService() {
         super.onNotificationPosted(sbn)
         Log.v(TAG, "new notification posted: ${sbn.toString()}")
 
-        if (sbn != null && sbn.packageName == "com.slipstream.accuradio"){
+        if (sbn != null && sbn.packageName == "com.slipstream.accuradio") {
 
             // parse the notificaion with refection
             val notification = sbn.notification
 
 
             var fields: ArrayList<Any>? = null
-            for(field in notification.contentView.javaClass.declaredFields){
-                if(field.name == "mActions"){
+            for (field in notification.contentView.javaClass.declaredFields) {
+                if (field.name == "mActions") {
                     field.isAccessible = true
                     fields = field.get(notification.contentView) as ArrayList<Any>
                 }
 
             }
 
-            Log.v(TAG, "some test")
             val info = java.util.LinkedList<String>()
             fields?.toArray()?.forEach {
-               if (it!= null){
-                  var fieldFilter = it.javaClass.declaredFields.filter{ it.name == "value"}
-                   if (fieldFilter.size == 1){
-                       var field = fieldFilter.get(0)
-                       field.isAccessible = true
+                if (it != null) {
+                    var fieldFilter = it.javaClass.declaredFields.filter { it.name == "value" }
+                    if (fieldFilter.size == 1) {
+                        var field = fieldFilter.get(0)
+                        field.isAccessible = true
 
-                       when(val _v = field.get(it)){
-                          is String -> info.push(_v)
-                          else -> "Not applicable"
-                       }
-                   }
-               }
+                        // necessary fields
+                        when (val _v = field.get(it)) {
+                            is String -> info.push(_v)
+                            else -> "Not applicable"
+                        }
+                    }
+                }
             }
 
-            // necessary fields
 
-            // mute
-            // unmute
+            if (info.size > 0 && audioManager is AudioManager) {
+                if (info.any { it.contains(getString(R.string.accuradio_ad_text)) }) {
+                    // mute
+                    audioManager?.adjustVolume(
+                        AudioManager.ADJUST_MUTE,
+                        AudioManager.FLAG_PLAY_SOUND
+                    )
+                    isMuted = true
+                    Log.v(TAG, "Add detected muting")
+                } else if (isMuted) {
+                    // unmute
+                    audioManager?.adjustVolume(
+                        AudioManager.ADJUST_UNMUTE,
+                        AudioManager.FLAG_PLAY_SOUND
+                    )
+                    isMuted = false
+                    Log.v(TAG, "Not an add: ${info.toString()}")
+                }
+            }
+
         }
 
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification?) {
         super.onNotificationRemoved(sbn)
+        //todo: check for accuradio notification present or not.
         Log.v(TAG, "notification removed: ${sbn.toString()}")
     }
 
