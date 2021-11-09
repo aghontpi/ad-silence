ff9b67d1756b0c071bfebe87c15ab8804fe10add
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-09 21:48:57 +0530

    Update UI to new theme

--- app/src/main/java/bluepie/ad_silence/MainActivity.kt
+++ app/src/main/java/bluepie/ad_silence/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val audioManager: AudioManager =
