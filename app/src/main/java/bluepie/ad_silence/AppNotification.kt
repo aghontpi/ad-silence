a5ca656c0ea7961885185c4f0f97b567477cb13a
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-11-28 12:22:18 +0530

    add: access the app, through notification

--- app/src/main/java/bluepie/ad_silence/AppNotification.kt
+++ app/src/main/java/bluepie/ad_silence/AppNotification.kt
@@ -3,8 +3,10 @@ package bluepie.ad_silence
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
@@ -43,11 +45,14 @@ fun AppNotificationHelper.getNotificationBuilder(status: String ): NotificationC
 }
 
 private fun AppNotificationHelper.createNotification(status: String): NotificationCompat.Builder{
+    val adSilenceIntent = Intent(context,AdSilenceActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context,0,adSilenceIntent, PendingIntent.FLAG_UPDATE_CURRENT)
     return NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
             .setSmallIcon(R.drawable.icon_launcher_foreground)
             .setContentTitle(context.getString(R.string.app_name))
             .setContentText(status)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
 }
 
 // android version > 26
