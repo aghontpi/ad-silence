99a102aed9ce019e88ed0bbedb60ead7115de232
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-12-03 01:11:37 +0530

    fix: spotify last word of ad is playing refactor Listener & Utils

--- app/src/main/java/bluepie/ad_silence/NotificationListener.kt
+++ app/src/main/java/bluepie/ad_silence/NotificationListener.kt
@@ -1,5 +1,6 @@
 package bluepie.ad_silence
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.media.AudioManager
@@ -7,80 +8,64 @@ import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
 
+@SuppressLint("LongLogTag")
 class NotificationListener : NotificationListenerService() {
     private val TAG = "NotificationListenerService"
-    private var mConnected = false
-    private var isMuted = false
     private var audioManager: AudioManager? = null
-    private var addNotificationHelper: AppNotificationHelper? = null
+    private var appNotificationHelper: AppNotificationHelper? = null
+    private var isMuted: Boolean = false
 
     override fun onCreate() {
         super.onCreate()
         audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+        appNotificationHelper = AppNotificationHelper(applicationContext)
         Log.v(TAG, "listener created")
-        addNotificationHelper = AppNotificationHelper(applicationContext)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val notificationBuilder =
-            addNotificationHelper?.getNotificationBuilder("adSilence, service started")
-        if (notificationBuilder != null) {
-            startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        appNotificationHelper?.getNotificationBuilder("adSilence, service started")?.run {
+            startForeground(NOTIFICATION_ID, this.build())
         }
         return Service.START_STICKY
     }
 
     override fun onListenerConnected() {
         super.onListenerConnected()
+        appNotificationHelper?.updateNotification("AdSilence, listening for ads")?.run {
+            startForeground(NOTIFICATION_ID, this) // persistent notification
+        }
         Log.v(TAG, "notification listener connected")
-        val notification = addNotificationHelper?.updateNotification("AdSilence, listening for ads")
-        mConnected = true
-
-        // persistent notification
-        startForeground(NOTIFICATION_ID, notification)
-    }
-
-    override fun onListenerDisconnected() {
-        super.onListenerDisconnected()
-        Log.v(TAG, "notification listener disconnected")
-        mConnected = false
     }
 
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
         sbn?.let {
             with(AppNotification(applicationContext, it.notification, sbn.packageName)) {
-                Preference(applicationContext).isAppConfigured(this.getApp()).takeIf { b->b }?.run{
-                    Log.v(TAG, "new notification posted: ${this@with.getApp()}")
-                    when (NotificationParser(this@with).isAd()) {
-                        true -> mute()
-                        false -> unMute()
+                Preference(applicationContext).isAppConfigured(this.getApp()).takeIf { b -> b }
+                    ?.run {
+                        Log.v(TAG, "new notification posted: ${this@with.getApp()}")
+                        Utils().run {
+                            when (NotificationParser(this@with).isAd()) {
+                                true -> {
+                                    Log.v(TAG, "Ad detected muting")
+                                    this.mute(audioManager, appNotificationHelper)
+                                    isMuted = true
+                                }
+                                false -> {
+                                    Log.v(TAG, "Not an ad")
+                                    isMuted.takeIf { b -> b }?.also {
+                                        isMuted = false
+                                        this@run.unmute(
+                                            audioManager,
+                                            appNotificationHelper,
+                                            this@with.getApp()
+                                        )
+                                    }
+                                }
+                            }
+                        }
                     }
-                }
             }
         }
-
-
     }
-
-    private fun mute() {
-        audioManager?.adjustVolume(
-            AudioManager.ADJUST_MUTE,
-            AudioManager.FLAG_PLAY_SOUND
-        )
-        isMuted = true
-        addNotificationHelper?.updateNotification("AdSilence, ad-detected")
-        Log.v(TAG, "Ad detected muting")
-    }
-
-    private fun unMute() {
-        audioManager?.adjustVolume(
-            AudioManager.ADJUST_UNMUTE,
-            AudioManager.FLAG_PLAY_SOUND
-        )
-        addNotificationHelper?.updateNotification("AdSilence, listening for ads")
-        Log.v(TAG, "Not an ad")
-        isMuted = false
-    }
-
 }
--- app/src/main/java/bluepie/ad_silence/Utils.kt
+++ app/src/main/java/bluepie/ad_silence/Utils.kt
@@ -2,34 +2,80 @@ package bluepie.ad_silence
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.media.AudioManager
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Switch
 
 class Utils {
-   private val TAG = "Utils"
+    private val TAG = "Utils"
 
-   fun disableSwitch(toggle: Switch){
-      toggle.isChecked = false
-      toggle.isEnabled =  false
-   }
+    fun disableSwitch(toggle: Switch) {
+        toggle.isChecked = false
+        toggle.isEnabled = false
+    }
 
-   fun enableSwitch(toggle: Switch){
-      toggle.isChecked = true
-      toggle.isEnabled = true
-   }
+    fun enableSwitch(toggle: Switch) {
+        toggle.isChecked = true
+        toggle.isEnabled = true
+    }
 
-   private fun isPackageInstalled(context: Context, packageName: String): Boolean{
-      return try {
-          context.packageManager.getPackageInfo(packageName,0)
-          true
-      } catch (e: PackageManager.NameNotFoundException){
-         Log.v(TAG, "exception: $e" )
-         false
-      }
-   }
+    private fun isPackageInstalled(context: Context, packageName: String): Boolean {
+        return try {
+            context.packageManager.getPackageInfo(packageName, 0)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.v(TAG, "exception: $e")
+            false
+        }
+    }
 
-   fun isAccuradioInstalled(context: Context) = isPackageInstalled(context, context.getString(R.string.accuradio_pkg_name))
+    fun isAccuradioInstalled(context: Context) =
+        isPackageInstalled(context, context.getString(R.string.accuradio_pkg_name))
 
-   fun isSpotifyInstalled(context: Context) = isPackageInstalled(context, context.getString(R.string.spotify_package_name))
+    fun isSpotifyInstalled(context: Context) =
+        isPackageInstalled(context, context.getString(R.string.spotify_package_name))
+
+    fun mute(audioManager: AudioManager?, addNotificationHelper: AppNotificationHelper?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            audioManager?.adjustVolume(
+                AudioManager.ADJUST_MUTE,
+                AudioManager.FLAG_PLAY_SOUND
+            )
+        } else {
+            audioManager?.setStreamMute(AudioManager.STREAM_MUSIC, true)
+        }
+        addNotificationHelper?.updateNotification("AdSilence, ad-detected")
+    }
+
+    fun unmute(
+        audioManager: AudioManager?,
+        addNotificationHelper: AppNotificationHelper?,
+        app: SupportedApps,
+    ) {
+        val process = {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                audioManager?.adjustVolume(
+                    AudioManager.ADJUST_UNMUTE,
+                    AudioManager.FLAG_PLAY_SOUND
+                )
+            } else {
+                audioManager?.setStreamMute(AudioManager.STREAM_MUSIC, false)
+            }
+            addNotificationHelper?.updateNotification("AdSilence, listening for ads")
+        }
+
+        if (app == SupportedApps.SPOTIFY) {
+            Log.v(TAG, "introducing delay for spotify")
+            Handler(Looper.getMainLooper()).postDelayed({
+                Log.v(
+                    TAG,
+                    "running unmute..after delay"
+                ); process()
+            }, 480)
+        } else process()
+    }
 }
 
