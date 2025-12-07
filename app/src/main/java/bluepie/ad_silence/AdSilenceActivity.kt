58ad895eb6b1c57a7c12b7f405bc98c3f2363706
bluepie <iambluepie@gmail.com>
2025-12-08 04:08:38 +0530

    feat: remove custom add app from main activity, opting to use the select apps option

--- app/src/main/java/bluepie/ad_silence/AdSilenceActivity.kt
+++ app/src/main/java/bluepie/ad_silence/AdSilenceActivity.kt
@@ -342,9 +342,7 @@ class AdSilenceActivity : Activity() {
             }
         }
 
-        findViewById<Button>(R.id.add_custom_app_btn)?.setOnClickListener {
-            showAddCustomAppDialog()
-        }
+
 
         findViewById<Button>(R.id.debug_log_btn)?.setOnClickListener {
             showDebugLogDialog()
