8976be8d77e3b2c888b6a74fd241486ac84952fd
bluepie <iambluepie@gmail.com>
2025-12-26 01:21:24 +0530

    feat: ability to edit preloaded configs

---
+++ app/src/main/java/bluepie/ad_silence/DefaultAppConfig.kt
@@ -0,0 +1,28 @@
+package bluepie.ad_silence
+
+import android.content.Context
+import bluepie.ad_silence.triggers.spotifyTrigger
+
+object DefaultAppConfig {
+    fun getDefaultKeywords(app: SupportedApps, context: Context): List<String> {
+        return when (app) {
+            SupportedApps.ACCURADIO -> listOf(context.getString(R.string.accuradio_ad_text))
+            SupportedApps.SPOTIFY, SupportedApps.SPOTIFY_LITE -> listOf(
+                context.getString(R.string.spotify_ad_string),
+                context.getString(R.string.spotify_ad2),
+                *spotifyTrigger
+            )
+            SupportedApps.TIDAL -> listOf(context.getString(R.string.tidal_ad_string))
+            SupportedApps.PANDORA -> listOf(
+                context.getString(R.string.pandora_ad_string),
+                context.getString(R.string.pandora_ad_string_2)
+            )
+            SupportedApps.LiveOne -> listOf(
+                context.getString(R.string.liveOne_ad_string),
+                context.getString(R.string.liveOne_ad_string_2)
+            )
+            SupportedApps.Soundcloud -> listOf(context.getString(R.string.soundcloud_ad_string))
+            else -> emptyList()
+        }
+    }
+}
